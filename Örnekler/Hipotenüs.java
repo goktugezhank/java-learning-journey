6b7e3182607792a80ca8daf2fc28542e0701cbcc
goktugezhank <goktugezhank@gmail.com>
2026-03-03 11:08:21 +0300

    Ornekler Klasorune yeni proje eklendi

--- Örnekler/Hipotenüs.java
+++ Örnekler/Hipotenüs.java
@@ -1,3 +1,13 @@
+import java.util.Scanner;
+
 void main() {
-  $END$
+    int a, b;
+    double c;
+    Scanner imp = new Scanner(System.in);
+    System.out.print("ilk kenarı gir");
+    a = imp.nextInt();
+    System.out.print("ikinci kenarı gir");
+    b = imp.nextInt();
+    c = Math.sqrt((a * a) + (b * b));
+    System.out.println("Hipotenüs = " + c);
 }
