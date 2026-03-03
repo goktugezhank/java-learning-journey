099f3c9a9fa79bd2d7edbd496ee44cd71ae63e0d
goktugezhank <49092701+goktugezhank@users.noreply.github.com>
2026-03-03 15:05:58 +0300

    Add taxi fare calculation based on distance

--- Örnekler/TaksiMetre.java
+++ Örnekler/TaksiMetre.java
@@ -1,3 +1,14 @@
 void main() {
-  $END$
+ int km;
+    double tl;
+    Scanner imp = new Scanner(System.in);
+    System.out.println("Kaç km gittiniz:");
+    km = imp.nextInt();
+    if (km <= 20) {
+        tl = 20;
+    } else
+        tl = km * 2.20 + 10;
+
+
+    System.out.println("Ücret=" + tl);
 }
